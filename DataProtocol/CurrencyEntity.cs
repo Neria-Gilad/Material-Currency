ab6dddb78f7d13a0842e8278a9d71e8b6cddaeff
Gilad Weiss <33327017+weissgilad@users.noreply.github.com>
2018-03-19 23:36:40 +0200

    Add files via upload

---
+++ DataProtocol/CurrencyEntity.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+// ReSharper disable InconsistentNaming
+
+namespace DataProtocol {
+    public enum CurrencyType {
+        USD, EUR, GBP, ILS, BTC, JMD
+    }
+
+    public class Quotes {
+
+        public double USDEUR { get; set; }
+
+        public double USDGBP { get; set; }
+
+        public double USDILS { get; set; }
+
+        public double USDBTC { get; set; }
+
+        public double USDJMD { get; set; }
+    }
+
+    public class LiveCurrencyEntity {
+
+        public string success { get; set; }
+
+        public string terms { get; set; }
+
+        public string privacy { get; set; }
+
+        public int timestamp { get; set; }
+
+        public string source { get; set; }
+
+        public Quotes quotes { get; set; }
+
+        /// <summary>
+        /// get the respective value from the qoutes
+        /// </summary>
+        /// <param name="type">currency code</param>
+        /// <returns>value for the chosen currency from qoute</returns>
+        public double GetValue(string type)
+        {
+            switch (type) {
+                case "USD": return 1;
+                case "BTC": return quotes.USDBTC;
+                case "EUR": return quotes.USDEUR;
+                case "ILS": return quotes.USDILS;
+                case "JMD": return quotes.USDJMD;
+                case "GBP": return quotes.USDGBP;
+                default: return -1; // Error occured
+            }
+        }
+    }
+
+    public class HistoricalCurrencyEntity : LiveCurrencyEntity {
+
+        public string historical { get; set; }
+
+        [Key]
+        public string date { get; set; }
+
+    }
+}
---
+++ DataProtocol/CurrencyFields.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace DataProtocol
+{
+    /// <summary>
+    /// defines a currency with its value
+    /// </summary>
+    public class CurrencyFields
+    {
+     
+        public static Dictionary<string, string> SymbolsDictionary = new Dictionary<string, string>()
+        {
+            {"USD", "$"},
+            {"EUR", "€"},
+            {"GBP", "£"},
+            {"ILS", "₪"},
+            {"BTC", "Ƀ"},
+            {"JMD", "J$"}
+        };
+        public static Dictionary<string, string> NameDictionary = new Dictionary<string, string>()
+        {
+            {"USD", "US Dollar"},
+            {"EUR", "Euro"},
+            {"GBP", "British Pound"},
+            {"ILS", "IL New Shekel"},
+            {"BTC", "Bitcoin"},
+            {"JMD", "Jamaican Dollar"}
+        };
+        public string Code { get; set; }
+        public double Value { get; set; }
+    }
+}
