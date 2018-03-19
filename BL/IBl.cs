fefb6764759c65456366a61a61c42c5b69b4742a
Gilad Weiss <33327017+weissgilad@users.noreply.github.com>
2018-03-19 23:33:06 +0200

    Add files via upload

---
+++ BL/IBl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DataProtocol;
+
+namespace BL
+{
+    public interface IBl
+    {
+        /// <summary>
+        /// an async function that returns all of the currency types in <see cref="CurrencyFields"/> format
+        /// </summary>
+        /// <returns>List of CurrencyFields which includes all the supported currencies</returns>
+        Task<List<CurrencyFields>> GetAllCurrenciesAsync();
+
+        /// <summary>
+        /// converts currencies from one type to another
+        /// </summary>
+        /// <param name="fromCurrency">code of source currency</param>
+        /// <param name="toCurrency">code of destination currency</param>
+        /// <returns>returns the ratio SRC/DST </returns>
+        Task<double> ConvertAsync(string fromCurrency, string toCurrency);
+
+        /// <summary>
+        /// converts a type of currency into all the other types
+        /// </summary>
+        /// <param name="from">source currency</param>
+        /// <param name="mult">multiplier eg how much of the source currency</param>
+        /// <returns>List of <see cref="CurrencyFields"/> with values matching the correct ratio</returns>
+        Task<List<CurrencyFields>> ConvertCurrencyListAsync(string from, double mult);
+
+        /// <summary>
+        /// gets updated current values of all the currency types
+        /// </summary>
+        /// <returns>an updated list of all the currency values</returns>
+        Task<List<CurrencyFields>> GetLiveListAsync();
+
+        /// <summary>
+        /// get currencies within a number of days from the current date
+        /// </summary>
+        /// <param name="days">number of days ago to start</param>
+        /// <returns>list of <see cref="HistoricalCurrencyEntity"/> from all the days starting from the requested date</returns>
+        Task<List<HistoricalCurrencyEntity>> GetTimeSpanEntityAsync(int days);
+    }
+}
---
+++ BL/bl.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataProtocol;
+using DAL;
+
+namespace BL {
+    public class Bl : IBl
+    {
+
+        /// <summary>
+        /// an async function that returns all of the currency types in <see cref="CurrencyFields"/> format
+        /// </summary>
+        /// <returns>List of CurrencyFields which includes all the supported currencies</returns>
+        public async Task<List<CurrencyFields>> GetAllCurrenciesAsync()
+        {
+            return await Task.Factory.StartNew(() => (
+                from cur in Enum.GetNames(typeof(CurrencyType))
+                select new CurrencyFields { Code = cur, Value = -1 }).ToList());
+        }
+
+        /// <summary>
+        /// converts currencies from one type to another
+        /// </summary>
+        /// <param name="fromCurrency">code of source currency</param>
+        /// <param name="toCurrency">code of destination currency</param>
+        /// <returns>returns the ratio SRC/DST </returns>
+        public async Task<double> ConvertAsync(string fromCurrency, string toCurrency)
+        {
+            HistoricalCurrencyEntity currentInfo = await new Dal().GetCurrencyByDay(DateTime.Today);
+            double from = 1 / currentInfo.GetValue(fromCurrency); // 1/(USD/from) = from/USD
+            double to = currentInfo.GetValue(toCurrency); //USD/to   
+            return from * to; // from/USD * USD/to = from/to
+        }
+
+        /// <summary>
+        /// converts a type of currency into all the other types
+        /// </summary>
+        /// <param name="from">source currency</param>
+        /// <param name="mult">multiplier eg how much of the source currency</param>
+        /// <returns>List of <see cref="CurrencyFields"/> with values matching the correct ratio</returns>
+        public async Task<List<CurrencyFields>> ConvertCurrencyListAsync(string from, double mult)
+        {
+            var ret = new List<CurrencyFields>();
+            foreach (var cur in Enum.GetNames(typeof(CurrencyType)).ToList()) {
+                ret.Add(new CurrencyFields { Code = cur, Value = (await ConvertAsync(from, cur)) * mult });
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// gets updated current values of all the currency types
+        /// </summary>
+        /// <returns>an updated list of all the currency values</returns>
+        public async Task<List<CurrencyFields>> GetLiveListAsync()
+        {
+            LiveCurrencyEntity res = await new Dal().GetLiveCurrencyAsync();
+            return await Task.Factory.StartNew(
+                () => (
+                from cur in Enum.GetNames(typeof(CurrencyType))
+                select new CurrencyFields { Code = cur, Value = 1/res.GetValue(cur) }).ToList()
+                );
+        }
+
+        /// <summary>
+        /// get currencies within a number of days from the current date
+        /// </summary>
+        /// <param name="days">number of days ago to start</param>
+        /// <returns>list of <see cref="HistoricalCurrencyEntity"/> from all the days starting from the requested date</returns>
+        public async Task<List<HistoricalCurrencyEntity>> GetTimeSpanEntityAsync(int days)
+        {
+            return (await new Dal().GetTimeSpanCurrency(days)).ToList();
+        }
+    }
+}
