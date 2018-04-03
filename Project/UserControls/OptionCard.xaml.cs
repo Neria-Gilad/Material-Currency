0c10b17b9e192c820d3259bd262dbcb8a8e69f76
Neria Tzidkani <neriat@gmail.com>
2018-04-03 21:25:46 +0300

    Add files via upload

---
+++ Project/UserControls/OptionCard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Project.UserControls
+{
+    /// <summary>
+    /// Interaction logic for OptionCard.xaml
+    /// </summary>
+    public partial class OptionCard : UserControl
+    {
+        public OptionCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project/UserControls/StockCard.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Project.UserControls
+{
+    /// <summary>
+    /// Interaction logic for StockCard.xaml
+    /// </summary>
+    public partial class StockCard : UserControl
+    {
+
+        public StockCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
